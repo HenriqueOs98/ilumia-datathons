0a789d537960736b27926f40e756b9ea4290b97b
Henrique Santos <henrique.santos@rouseservices.com>
2025-09-29 12:14:31 -0300

    feat(infra): add missing outputs to knowledge_base module

--- infra/modules/knowledge_base/outputs.tf
+++ infra/modules/knowledge_base/outputs.tf
@@ -2,3 +2,23 @@ output "opensearch_collection_endpoint" {
   description = "The endpoint of the OpenSearch Serverless collection"
   value       = aws_opensearchserverless_collection.knowledge_base.collection_endpoint
 }
+
+output "knowledge_base_id" {
+  description = "The ID of the Bedrock Knowledge Base"
+  value       = aws_bedrockagent_knowledge_base.ons_knowledge_base.id
+}
+
+output "knowledge_base_arn" {
+  description = "The ARN of the Bedrock Knowledge Base"
+  value       = aws_bedrockagent_knowledge_base.ons_knowledge_base.arn
+}
+
+output "opensearch_collection_arn" {
+  description = "The ARN of the OpenSearch Serverless collection"
+  value       = aws_opensearchserverless_collection.knowledge_base.arn
+}
+
+output "data_source_id" {
+  description = "The ID of the Bedrock Knowledge Base Data Source"
+  value       = aws_bedrockagent_data_source.s3_data_source.id
+}
